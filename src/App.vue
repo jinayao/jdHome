51450ce95d4c7413f7cf5e6b27ffa17d4a361749
chj <470945645@qq.com>
2022-09-12 16:29:44 +0800

    底部导航

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,47 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="wrapper">11</div>
+  <div class="docker">
+    <span class="docker__item docker__item--active">
+      <i class="iconfont">&#xe6cb;</i>
+      首页</span
+    >
+    <span class="docker__item"> <i class="iconfont">&#xe682;</i>购物车</span>
+    <span class="docker__item"> <i class="iconfont">&#xe897;</i>订单</span>
+    <span class="docker__item"> <i class="iconfont">&#xe8a0;</i>我的</span>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.wrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: .5rem;
+  right: 0;
+  background-color: yellow;
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+.docker {
+  display: flex;
+  position: absolute;
+  padding: 0 0.18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border-top: 1px solid #f1f1f1;
+  box-sizing: border-box;
+  &__item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex: 1;
+    color: #000;
+    .iconfont {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: 0.18rem;
+    }
+    &--active {
+      color: #1fa4fc;
     }
   }
 }
